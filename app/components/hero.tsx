ac24d7fcaec38c8aad975b335d88611aaae76a87
Daniel <dannyzny@gmail.com>
2024-08-10 15:16:44 -0400

    updated hero

---
+++ app/components/hero.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Hero(){
+    return (
+      <>
+        <div className="flex flex-col items-center h-[100vh] w-full bg-green-500 z-0 pt-72 px-20">
+          {/* <img src="Zauroff Website Hero.png" className="w-full md:max-w-[50vw] fixed px-20" alt="Hero" /> */}
+
+          <h1 className="fixed text-6xl">
+            Under Construction.
+          </h1>
+
+        </div>
+      </>
+    );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-0 z-10 relative">
-      <div className="mt-[25vh] h-32">
-        
-      </div>
-      <div className="flex justify-center items-center h-[100vh] w-full bg-green-500 -z-10 fixed top-1 pt-20">
-        <img src="Zauroff Website Hero.png" className="w-full md:max-w-[40vw]" alt="Hero" />
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-start p-0 relative">
+      <Hero></Hero>
+      
+      {/* <div className="bg-black h-[100vh] relative w-full">
+        Content for the black div
+      </div> */}
     </main>
   );
 }
