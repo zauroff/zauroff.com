36000fb9cb2f88b7703c86c9bfd2d057022ca2d3
Daniel <dannyzny@gmail.com>
2024-08-10 20:41:41 -0400

    hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,16 +1,50 @@
 
 
 export default function Hero(){
-    return (
-      <>
-        <div className="flex flex-col items-center h-[100vh] w-full bg-green-500 z-0 pt-72 px-20">
-          {/* <img src="Zauroff Website Hero.png" className="w-full md:max-w-[50vw] fixed px-20" alt="Hero" /> */}
+  return (
+    <>
+      <div className="flex flex-col items-center h-[100vh] w-full bg-green-500 z-0 pt-0 relative">
+        <div className="fixed inset-0 flex justify-center items-center">
+          <div className="relative h-[95vh] w-[95vw] md:h-[90vh] md:w-[90vw]">
+            <video 
+              src="front_video.mp4"
+              autoPlay
+              muted
+              playsInline
+              loop
+              className="rounded-[30px] border-2 border-black h-full w-full object-cover shadow-black shadow-md"
+            ></video>
 
-          <h1 className="fixed text-6xl">
-            Under Construction.
-          </h1>
-
+            {/* Image positioned inside the video */}
+            <img
+              src="logo.svg"
+              alt="zauroff"
+              className="absolute md:-left-4 left-6 bottom-6 opacity-95 h-[80px] w-[300px] md:h-[100px] md:w-[600px] max-h-[100vh]"
+            />
+          </div>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 };
+
+
+
+// export default function Hero(){
+//   return (
+//     <>
+//       <div className="flex flex-col items-center h-[100vh] w-full bg-green-500 z-0 pt-0">
+//         <div className="fixed inset-0 flex justify-center items-center">
+//           <video 
+//             src="zauroff front page video.mp4"
+//             autoPlay
+//             muted
+//             playsInline
+//             loop
+//             className="rounded-[30px] border-2 border-black h-[100vh] object-cover md:w-[100vw] md:h-auto"
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,38 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-0 relative">
       <Hero></Hero>
       
-      {/* <div className="bg-black h-[100vh] relative w-full">
-        Content for the black div
-      </div> */}
+      <div className="bg-black h-[100vh] relative w-full flex justify-center text-white">
+        <h1 className="text-5xl pt-12">Under Construction.</h1>
+        {/* Content for the black div */}
+        {/* <NavigationMenu >
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className=""></NavigationMenuTrigger>
+              <NavigationMenuContent className="flex flex-col px-20">
+                <NavigationMenuLink>One</NavigationMenuLink>
+                <NavigationMenuLink>Two</NavigationMenuLink>
+                <NavigationMenuLink>Three</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu> */}
+
+      </div>
     </main>
   );
 }
